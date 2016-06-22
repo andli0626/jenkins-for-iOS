762b51b794fe95395003e8a54ce5efde5bac694a
李林 <ww1095@163.com>
2016-06-22 22:57:03 +0800

    init

---
+++ jenkins test for iOS/jenkins test for iOS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  jenkins test for iOS
+//
+//  Created by 李林 on 16/6/22.
+//  Copyright © 2016年 李林. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ jenkins test for iOS/jenkins test for iOS/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  jenkins test for iOS
+//
+//  Created by 李林 on 16/6/22.
+//  Copyright © 2016年 李林. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
